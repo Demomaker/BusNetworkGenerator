8b7b4c784d7935ef42f3100d3447b480d6f1784b
Mike Bédard <mike01bedard@gmail.com>
2020-10-12 00:10:09 -0400

    interface v0.1

---
+++ src/net/demomaker/busnetworkgenerator/controller/ControllerBusNetworkGenerator.java
@@ -0,0 +1,13 @@
+package net.demomaker.busnetworkgenerator.controller;
+
+import net.demomaker.busnetworkgenerator.view.HomePage;
+
+import javax.swing.*;
+
+public class ControllerBusNetworkGenerator {
+
+    public static void main(String[] args) {
+        JFrame frame = new HomePage();
+        frame.setVisible(true);
+    }
+}
